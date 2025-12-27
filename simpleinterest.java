48d77e95f9efc38c93b0e1eff1c2aa84f33ffae8
krishna-lonare <krishnalonare143@gmail.com>
2025-12-27 15:58:39 +0530

    java progrmas

---
+++ simpleinterest.java
@@ -0,0 +1,14 @@
+public class simpleinterest {
+    public static void main(String[] args) {
+        
+        double p , r ,t ;
+        p =  5000;
+        r = 6.5;
+        t = 5;
+
+        double simpleinterest = (p*r*t)/100;
+
+        System.out.println("The simple interest is = " +(int)simpleinterest);
+
+    }
+}
